2f1c69657f52d72fcd1847e99dd8b84f81d9d047
JutIgor <jutigor@yandex.ru>
2015-02-09 19:54:39 +0300

    add final project

---
+++ Final Project/Irufushi/Abstract/Abstract/IUserPagesRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstract.Entities;
+
+namespace Abstract.Abstract
+{
+    public interface IUserPagesRepository
+    {
+        IQueryable<UserProfile> UserProfiles { get; }
+        void SaveUser(UserProfile user);
+        void SaveAboutUser(AboutUser about);
+        void SaveContacts(Contacts contacts);
+        void SaveLocation(Location location);
+        UserProfile GetUser(int id);
+        void SaveProfile(UserProfile user);
+        void AddFriend(FriendShip friendship);
+        void DeleteFriend(FriendShip friendship);
+        List<UserProfile> GetFriends(int id);
+        bool IsFriend(int idUser, int isFriend);
+        IQueryable<UserProfile> SearchUsersFName(string firstName, string lastName,
+            string country, string city, string skype);
+    }
+}
---
+++ Final Project/Irufushi/Irufushi.WebUI/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abstract.Abstract;
+using Abstract.Entities;
+using Irufushi.Domain.Concrete;
+using Irufushi.WebUI.Models;
+using WebMatrix.WebData;
+
+namespace Irufushi.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserPagesRepository _repository;
+        //
+        // GET: /User/
+
+        public UserController(IUserPagesRepository userRepository)
+        {
+            _repository = userRepository;
+        }
+
+        [Authorize]
+        public ActionResult Index(int? id)
+        {
+            if(id == null) id = WebSecurity.CurrentUserId;
+            UserProfile user = new UserProfile();
+            
+            user = _repository.GetUser((int)id);
+
+            if (id != WebSecurity.CurrentUserId)
+            {
+                ViewBag.Id = id;
+                ViewBag.Button = true;
+                if (_repository.IsFriend(WebSecurity.CurrentUserId, (int)id))
+                    ViewBag.TypeButton = false;
+                else ViewBag.TypeButton = true;
+            }
+            return View(user);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) id = WebSecurity.CurrentUserId;
+            UserProfile user = new UserProfile();
+            user = _repository.GetUser((int)id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Edit(UserProfile model, int? id)
+        {
+            if (id == null) id = WebSecurity.CurrentUserId;
+            if (model.AboutUser.Id == 0) model.AboutUser.Id = (int)id;
+            if(model.UserId == 0) model.UserId = (int)id;
+            if(model.Contacts.Id == 0) model.Contacts.Id = (int)id;
+            if (model.Location.Id == 0) model.Location.Id = (int)id;
+
+            _repository.SaveProfile(model);
+
+            return RedirectToAction("Index", "User", new { id = (int)id });
+        }
+
+        [Authorize]
+        public ActionResult AddFriend(int id)
+        {
+            FriendShip friendship = new FriendShip();
+
+            friendship.UserId = WebSecurity.CurrentUserId;
+            friendship.FriendId = id;
+            _repository.AddFriend(friendship);
+
+            return RedirectToAction("Index", "User", new { id = id });
+        }
+
+        [Authorize]
+        public ActionResult DeleteFriend(int id)
+        {
+            FriendShip friendship = new FriendShip();
+
+            friendship.UserId = WebSecurity.CurrentUserId;
+            friendship.FriendId = id;
+
+            _repository.DeleteFriend(friendship);
+
+            return RedirectToAction("Index", "User", new { id = id });
+        }
+
+        [Authorize]
+        public ActionResult ShowFriends(int? id)
+        {
+            if(id == null) id = WebSecurity.CurrentUserId;
+
+            UserModel viewModel = new UserModel
+            {
+                Users = _repository.GetFriends((int)id)
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        public ActionResult ShowAllUsers()
+        {
+            List<UserProfile> userWM = _repository.UserProfiles.ToList();
+
+            userWM.Remove(_repository.GetUser(WebSecurity.CurrentUserId));
+
+            UserModel viewModel = new UserModel
+            {
+                Users = userWM
+            };
+
+            return View(viewModel);
+        }
+    }
+}
